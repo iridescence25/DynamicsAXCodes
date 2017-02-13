c52acb2d4c180556da7027541abfb3bb0e01e5da
Harvey Oroceo <iridescence.cube@outlook.ph>
2017-02-13 14:03:37 +0800

    Create classDeclaration.cs

---
+++ SampleReport/UIBuilderClass/classDeclaration.cs
@@ -0,0 +1,8 @@
+class UIBuilderClass extends SrsReportDataContractUIBuilder
+{
+    //Specify the contract instance
+    Contract      contract;
+    
+    //Specify the fields to be used for the dialog/parameter box of the report
+    DialogField   dialogSite, dialogFromDate, dialogToDate, dialogItemId, dialogFormulaItem, dialogLocation, dialogShift;
+}
