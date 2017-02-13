a21585603e1895537e0ab7bbd8f29d1957bcaa29
Harvey Oroceo <iridescence.cube@outlook.ph>
2017-02-13 15:54:48 +0800

    Create parmToDate.cs

---
+++ SampleReport/UIBuilderClass/parmToDate.cs
@@ -0,0 +1,9 @@
+[
+    DataMemberAttribute,
+    SysOperationLabelAttribute(literalstr("CUS0006"))
+]
+public ToDate parmTodate(ToDate _toDate = toDate)
+{
+    toDate = _toDate;
+    return toDate;
+}
