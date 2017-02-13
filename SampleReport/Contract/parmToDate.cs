63fa87f81fcf9953705ba91d87e8e5eadff69ed4
Harvey Oroceo <iridescence.cube@outlook.ph>
2017-02-13 16:04:51 +0800

    Create parmToDate.cs

---
+++ SampleReport/Contract/parmToDate.cs
@@ -0,0 +1,9 @@
+[
+    DataMemberAttribute,
+    SysOperationLabelAttribute(literalstr("@SYS0008"))
+]
+public ToDate parmTodate(ToDate _toDate = toDate)
+{
+    toDate = _toDate;
+    return toDate;
+}
