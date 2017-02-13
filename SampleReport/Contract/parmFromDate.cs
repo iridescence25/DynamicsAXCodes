12657a83a4c0f95192849593eec6e03193810114
Harvey Oroceo <iridescence.cube@outlook.ph>
2017-02-13 15:47:21 +0800

    Create parmFromDate.cs

---
+++ SampleReport/Contract/parmFromDate.cs
@@ -0,0 +1,9 @@
+[
+    DataMemberAttribute,
+    SysOperationLabelAttribute(literalstr("@CUS0002"))
+]
+public FromDate parmFromDate(FromDate _fromDate = fromDate)
+{
+    fromDate = _fromDate;
+    return fromDate;
+}
